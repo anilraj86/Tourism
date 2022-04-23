c9095fd4600602b9e0a5fce00988af2fe63a0db7
anilraj86 <anilaryaa1006@gmail.com>
2022-04-23 11:42:39 +0400

    changes

--- js/myValidation.js
+++ js/myValidation.js
@@ -37,7 +37,7 @@ function loginValidate()
         {
             //alert(str);
             document.getElementById('emailAddr').style.borderColor="black";    
-        }
+         }
         else
         {
         document.getElementById('emailAddr').style.borderColor="red";
@@ -59,6 +59,7 @@ function loginValidate()
 
     function mobileNo()
     {
+        document.getElementById('mobileNo').style.borderColor="black";
         var mobnopat=/^([0-9]{3})?[- .]?([0-9]{3})[- .]?([0-9]{4})$/;
         var mobno=document.getElementById('mobileNo').value;
         if(!mobnopat.test(mobno))
@@ -70,9 +71,10 @@ function loginValidate()
     }
     function passwordCheck()
     {
-        var weakPassword=/^(?=.*[A-Z])(?=.*[a-z])([A-Za-z]{8,})$/;
-        var moderatePassword=/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])([A-Za-z0-9]{8,})$/;
-        var strongpassword=/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])([A-Za-z0-9@!%*$#]{8,})$/;
+        // password pattern based on the strength
+        var weakPassword=/^(?=.*[A-Z])(?=.*[a-z])([A-Za-z]{8,})$/; // without number
+        var moderatePassword=/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])([A-Za-z0-9]{8,})$/; // without special characters
+        var strongpassword=/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])([A-Za-z0-9@!%*$#]{8,})$/; // including special character 
         var password=document.getElementById('password').value;
         //alert(password);
         let strengthBadge=document.getElementById('StrengthDisp');
@@ -98,7 +100,7 @@ function loginValidate()
         else if(password!="")
         {
             strengthBadge.style.backgroundColor="blue";
-            strengthBadge.textContent="Password does not meet the requirements";
+            strengthBadge.textContent="Password Should be 8 character long and contains A-Z, a-z, 0-9, @!%*$# ";
         }
    
     }
